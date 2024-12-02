9da1a728869bc2af7e542e18ababfd75c171c994
abellows50 <abellows50@stuy.edu>
2024-12-02 17:29:15 -0500

    added basic basic prompt

--- shell.c
+++ shell.c
@@ -3,26 +3,47 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void sighandler(int signo){
+  if(signo==SIGINT){
+
+  }
+}
 
 int main(){
+  signal(SIGINT, sighandler);
   while (1){
+
+    //print prompt
+    printf(">> ");
+    fflush(stdout);
+
+    //get cmd
     char line[256];
-    fgets(line, 256, stdin);
-    line[strlen(line) - 1] = 0;
+    if(fgets(line, 256, stdin)){
+      line[strlen(line) - 1] = 0;
+      char * arg_ary[256];
+      parse_args(line, arg_ary);
 
-    printf("%s\n", line);
+      // if it was exit stop the program
+      if(!strcmp(arg_ary[0], "exit")){
+        exit(0);
+      }
 
-
-    char * arg_ary[256];
-    parse_args(line, arg_ary);
-
-
-    if(!strcmp(arg_ary[0], "exit")){
-      exit(0);
+      // fork
+      if(!fork()){
+        execvp(arg_ary[0], arg_ary);
+      }
+      else{
+        int status;
+        int kidid = wait(&status); //wait for child
+      }
     }
-
-    if(!fork()){
-      execvp(arg_ary[0], arg_ary);
+    else{ //if EOF exit
+      exit(0);
     }
   }
 }
