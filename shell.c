61be26a50f5fc194e019302b0bfca6efeeec57ef
Abel Bellows <abellows50@stuy.edu>
2024-12-02 14:43:26 -0500

    +drafts

--- shell.c
+++ shell.c
@@ -1,12 +1,12 @@
 #include "parse.h"
 #include <stdio.h>
 #include <unistd.h>
-#include <stlib.h>
+#include <stdlib.h>
 
 int main(){
   while (1){
     char line[256];
     fgets(line, 256, stdin);
-    
+
   }
 }
