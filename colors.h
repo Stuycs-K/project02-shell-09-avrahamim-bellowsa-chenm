2596bd0e142fba20c508cf23fff025aacdbacbea
abellows50 <abellows50@stuy.edu>
2024-12-03 09:12:30 -0500

    +colors.h file

---
+++ colors.h
@@ -0,0 +1,11 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define RED "\e[31m"
+#define BLUE "\e[34m"
+#define GREEN "\e[32m"
+#define BLINK "\e[5m"
+#define BLINKEND ""
+#define COLOREND "\e[0m"
+
+#endif
