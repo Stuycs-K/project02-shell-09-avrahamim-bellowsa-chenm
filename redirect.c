82dd7362f6f34b1a861d07031218bfda90b9b8c6
abellows50 <abellows50@stuy.edu>
2024-12-03 16:56:05 -0500

    started redirect

--- redirect.c
+++ redirect.c
@@ -6,3 +6,34 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <fcntl.h>
+
+#include "redirect.h"
+
+int redirect_stdout(int fd){
+    int old_stdout = dup(stdout);
+    dup2(fd, stdout);
+    return old_stdout;
+}
+
+int redirect_stdout_create_file(char * name){
+    int fd = open(name, O_CREAT | O_TRUNC );
+    return redirect_stdout(fd);
+}
+
+int redirect_stdin(int fd){
+    int old_stdin = dup(stdin);
+    dup2(fd, stdin);
+    return old_stdin;
+}
+
+int redirect_stdout_create_file(char * name){
+    int fd = open(name, O_CREAT | O_TRUNC );
+    return redirect_stdin(fd);
+}
+
+
+void reset_fds(int old_stdin, int old_stdout){
+    dup2(old_stdin, stdin);
+    dup2(old_stdout, stdout);
+}
---
+++ redirect.h
@@ -0,0 +1,12 @@
+#ifndef REDIRECT_H
+#define REDIRECT_H
+
+int redirect_stdout(int fd);
+int redirect_stdout_create_file(char * name);
+
+int redirect_stdin(int fd);
+int redurect_stdin_create_file(char * name);
+
+int reset_fds(int old_stdin, int old_stdout);
+
+#endif
