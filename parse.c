e70f349886aef6c0c4af58f6aaa96fe5bed844e4
abellows50 <abellows50@stuy.edu>
2024-12-04 16:23:53 -0500

    starting parse.c saving first draft before deletion

--- parse.c
+++ parse.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -22,4 +23,35 @@ void parse_lines(char * input, char ** line_ary){
     line_ary[i] = strsep(&input,";");
   }
   line_ary[i] = NULL;
+}
+
+void parse_opps(char * line, char ** opp_seperated_ary){
+  //goal is to sep on <, >, |
+  // a < b | c > d -> {a,<,b,|,c,>,d}
+
+
+  char * greater_char = ">";
+  char * less_char = "<";
+  char * pipe_char = "|";
+
+  char * symbolv[3];
+  symbolv[0] = greater_char;
+  symbolv[1] = less_char;
+  symbolv[2] = pipe_char;
+
+  unsigned int line_start = (unsigned int) line;
+
+  unsigned int first_greater = ( unsigned int ) strchrnul(line,'>') - line_start;
+  unsigned int first_less = ( unsigned int ) strchrnul(line, '<') - line_start;
+  unsigned int first_pipe = ( unsigned int ) strchrnul(line, '|') - line_start;
+
+  char * start = NULL;
+
+
+  printf("%d,  >: %u, <: %u, |: %u\n", strlen(line), first_greater, first_less, first_pipe);
+
+}
+
+int main(){
+  parse_opps("|  ", NULL);
 }
--- parse.h
+++ parse.h
@@ -1,5 +1,6 @@
 #ifndef PARSE_H
 #define PARSE_H
-void parse_args( char * line, char ** arg_ary );
+void parse_args( char * line, char ** arg_ary ); //parse the args fir execvp
 void parse_lines(char * input, char ** line_ary); //parses set of semicolon seperated lines into ary
+void parse_opps(char * line, char ** opp_seperated_ary); //parse a cmd sequence with redirects into a ary
 #endif
